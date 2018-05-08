2b886d87e23606bff8bfcd14b17ff06a2482dbf1
CuriousCat-7 <limingyao128@163.com>
2018-05-08 22:36:56 +0800

    dataset

--- readFiles.py
+++ readFiles.py
@@ -3,6 +3,7 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.image
 import os
 
+#数据
 #sequence 00 as probe, sequence 01 as gallery, both are used for training
 #X:data, Y:id, p:probe, g:gallery [00]:angle
 Xp_00=[]
@@ -19,15 +20,18 @@ Yg_90=[]
 
 id_list = np.zeros([15,2]).astype(int)#################################
 
+#路径
 root_dir = os.path.join("../input")
 Xp_00_dir = os.path.join(root_dir, "000-00", "000-00")
 Xg_00_dir = os.path.join(root_dir, "000-01", "000-01")
 Xp_90_dir = os.path.join(root_dir, "090-00", "090-00")
 Xg_90_dir = os.path.join(root_dir, "090-01", "090-01")
 
+#id_table: 每个id对应GEI在矩阵中的索引，不存在的为-1 [id,Xp_00,Xg_00,Xp_90,Xp_90]
 id_table=np.ones([id_list.shape[0],5])*(-1)
 id_table[:,0]=id_list[:,0]
 
+#读取数据
 for i in range(0,id_list.shape[0]):
     id_list[i,0] = (i*2+1)#################################
     id_table[i,0]=id_list[i,0]#################################
